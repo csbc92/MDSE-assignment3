5a99953fe96c843f730288aacff7ac47dacf684e
Christian Clausen <csbc92@hotmail.com>
2020-04-27 23:17:30 +0200

    Added support for functional style

--- MathAssignment3/src-gen/MathComputation.java
+++ MathAssignment3/src-gen/MathComputation.java
@@ -1,6 +1,5 @@
 /*
-* -- AUTO-GENERATED CODE --
-* --   DO NOT MODIFY!    --
+* AUTO-GENERATED CODE!
 */
 public class MathComputation {
 	
@@ -33,6 +32,12 @@ public class MathComputation {
 		System.out.println("This is my calc 1 " + computeThisismycalc1());
 		System.out.println("This_is 2nd calculation " + computeThis_is2ndcalculation());
 		System.out.println("this is my 3rd calculation " + computeThisismy3rdcalculation());
+		System.out.println("First value " + computeFirstvalue());
+		System.out.println("This is my calc 2 " + computeThisismycalc2());
+		System.out.println("another calc " + computeAnothercalc());
+		System.out.println("Second value " + computeSecondvalue());
+		System.out.println("Third value " + computeThirdvalue());
+		System.out.println("Third value2 " + computeThirdvalue2());
 	}
 	
 	/*
@@ -47,11 +52,89 @@ public class MathComputation {
 	private int computeThisismy3rdcalculation() {
 		return ((1)+((externals.power(((2)), ((3))))*(externals.sqrt(((9))))));
 	}
+	private int computeFirstvalue() {
+		return (new Function<Integer, Integer>() {
+			@Override
+			public Integer apply(Integer t) {
+				return ((2)*(new Function<Integer, Integer>() {
+					@Override
+					public Integer apply(Integer t) {
+						return ((t)+(1));
+					}
+				}.apply(((t)*(2)))));
+			}
+		}.apply(((1)+(2))));
+	}
+	private int computeThisismycalc2() {
+		return (((1)+(((6)/(3))*(5)))-(new Function<Integer, Integer>() {
+			@Override
+			public Integer apply(Integer t) {
+				return (t);
+			}
+		}.apply((1))));
+	}
+	private int computeAnothercalc() {
+		return (new Function<Integer, Integer>() {
+			@Override
+			public Integer apply(Integer t) {
+				return ((t)+(1));
+			}
+		}.apply((((2)*(5))+((4)/(2)))));
+	}
+	private int computeSecondvalue() {
+		return (new Function<Integer, Integer>() {
+			@Override
+			public Integer apply(Integer t) {
+				return (new Function<Integer, Integer>() {
+					@Override
+					public Integer apply(Integer t) {
+						return ((t)+(1));
+					}
+				}.apply(((t)*(2))));
+			}
+		}.apply(((2)+(2))));
+	}
+	private int computeThirdvalue() {
+		return ((new Function<Integer, Integer>() {
+			@Override
+			public Integer apply(Integer t) {
+				return (new Function<Integer, Integer>() {
+					@Override
+					public Integer apply(Integer t) {
+						return ((t)+(1));
+					}
+				}.apply(((t)*(2))));
+			}
+		}.apply(((3)+(2))))*(1));
+	}
+	private int computeThirdvalue2() {
+		return ((new Function<Integer, Integer>() {
+			@Override
+			public Integer apply(Integer t) {
+				return (new Function<Integer, Integer>() {
+					@Override
+					public Integer apply(Integer t) {
+						return ((t)+((1)*(externals.sqrt(((4))))));
+					}
+				}.apply((((t)*(2))+(externals.power(((t)), ((2)))))));
+			}
+		}.apply(((3)+((2)*(externals.sqrt(((9))))))))*(1));
+	}
 	
 	/*
 	* Main methods
 	*/
 	public static void main(String[] args) {
-		new MathComputation().compute();
+		new MathComputation(new Externals() {
+			@Override
+			public int power(int a, int b) {
+				// TODO: Implement method
+				throw new UnsupportedOperationException();
+			}
+			public int sqrt(int a) {
+				// TODO: Implement method
+				throw new UnsupportedOperationException();
+			}
+		}).compute();
 	}
 }
